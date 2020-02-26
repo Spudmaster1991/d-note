0ad6bc6442614f8ef77590da3e7f91951c310da4
Dallen Bishop <dallen.bishop@uvu.edu>
2020-02-26 13:06:48 -0700

    Declare a relative path to the wordlist

--- dnote/password_api.py
+++ dnote/password_api.py
@@ -1,9 +1,9 @@
+import os
 import random
 import string
 def generatepass(custom, wordlist, seperators, customChar, numbers, specialChar, exclude, letters):
     password = ""
-    #import wordlist
-    wordlist = 'C:/Users/Anthony/Documents/GitHub/password_reset/static/agwordlist.txt'
+    wordlist = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/agwordlist.txt')
     count = len(open(wordlist).readlines(  ))
     with open(wordlist) as wl:
         fullwordlist = wl.read().split('\n')
@@ -11,8 +11,8 @@ def generatepass(custom, wordlist, seperators, customChar, numbers, specialChar,
     #list of seperators used and selection of seperator
     sep = ["-","_",",","."," "]
     sep_used = sep[random.randint(0,len(sep)-1)]
-    
-    
+
+
     if custom != "":
         for element in range(0, len(custom)):
             #upper or lowercase word
@@ -65,7 +65,7 @@ def generatepass(custom, wordlist, seperators, customChar, numbers, specialChar,
     # count = len(open(wordlist).readlines(  ))
     # with open(wordlist) as wl:
     #     fullwordlist = wl.read().split('\n')
-    #     wl.close()    
+    #     wl.close()
     # word1 = fullwordlist[random.randint(0, count)]
     # word2 = fullwordlist[random.randint(0, count)]
     # word3 = fullwordlist[random.randint(0, count)]
@@ -73,4 +73,4 @@ def generatepass(custom, wordlist, seperators, customChar, numbers, specialChar,
     # seperators = ['.', '-', '*', '"']
 
     # password = word1 + str(random.randint(0, 9)) + seperators[random.randint(0, 3)] + word2 + str(random.randint(0, 9)) + seperators[random.randint(0, 3)] + word3 + str(random.randint(0, 9))
-    # return(password)
+    return password
