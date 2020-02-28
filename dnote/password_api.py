2c82458a014b065ae1b6f22bfbc3812638d53dd3
Dallen Bishop <dallen.bishop@uvu.edu>
2020-02-28 13:46:52 -0700

    Preliminary testing of multi operations

--- dnote/password_api.py
+++ dnote/password_api.py
@@ -2,43 +2,94 @@ import os
 import random
 from random import getrandbits
 import string
+
+wordlist = open(os.path.join(os.path.dirname(os.path.abspath(
+    __file__)), 'static/agwordlist.txt'), 'r').readlines()
+
 def generatepass(custom, exclude, charNumber):
     password = ""
-    wordlist = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/agwordlist.txt'), 'r').readlines()
+    # list of seperators used
+    sep = ["-", "_", ",", ".", " "]
 
-    #list of seperators used
-    sep = ["-","_",",","."," "]
+    # number of times to repeat a certain generation method
+    numRepeat = 0
 
-    for element in custom:
-        #upper or lowercase word
-        if element == "w":
-            if not getrandbits(1):
-                word = string.capitalize(wordlist[random.randint(0, len(wordlist) - 1)].strip())
-            else:
-                word = string.lower(wordlist[random.randint(0, len(wordlist) - 1)].strip())
-            password = password + word
-        #Seperator
-        elif element == "s":
-            password = password + sep[random.randint(0,len(sep) - 1)]
-        #number
-        elif element == "n":
-            password = password + random.choice(string.digits)
-        #Random Character
-        elif element == "r":
-            password = password + random.choice(string.ascii_letters + string.digits + string.punctuation)
-        #Alphanumeric Character
-        elif element == "a"
-            password = password + random.choice(string.ascii_letters + string.digits)
-        #Special Character
-        elif element == "S":
-            password = password + random.choice(string.punctuation)
-        #Upper or Lowercase Letter
-        elif element == "L":
-            if not getrandbits(1):
-                letter = string.upper(random.choice(string.ascii_letters))
-            else:
-                letter = string.lower(random.choice(string.ascii_letters))
-            password = password + letter
-        else: pass
-
+    for index, element in enumerate(custom):
+        if element == "*":
+            # Need to get the number after the star
+            end = index + 1
+            while custom[end] in string.digits:
+                ++end
+            numRepeat = int(custom[index + 1:end])
+            # Need to get the char of the operation to perform
+            while numRepeat > 0:
+                password = password + operations(index - 1)
+        elif element in string.digits:
+            pass
     return password
+
+
+def operations(op):
+    text = ''
+    # upper or lowercase word
+    if op == "w":
+        text = word()
+    # Seperator
+    elif op == "s":
+        text = seperator()
+    # number
+    elif op == "n":
+        text = number()
+    # Random Character
+    elif op == "r":
+        text = randomChar()
+    # Alphanumeric Character
+    elif op == "a"
+        text = alphanumeric()
+    # Special Character
+    elif op == "S":
+        text = specialChar()
+    # Upper or Lowercase Letter
+    elif op == "L":
+        text = letter()
+    else:
+        pass
+    return text
+
+
+def word(wordlist):
+    if not getrandbits(1):
+        word = string.capitalize(
+            wordlist[random.randint(0, len(wordlist) - 1)].strip())
+    else:
+        word = string.lower(
+            wordlist[random.randint(0, len(wordlist) - 1)].strip())
+    return word
+
+
+def seperator():
+    return sep[random.randint(0, len(sep) - 1)]
+
+
+def number():
+    return random.choice(string.digits)
+
+
+def randomChar():
+    return random.choice(string.ascii_letters + string.digits + string.punctuation)
+
+
+def alphanumeric():
+    return random.choice(string.ascii_letters + string.digits)
+
+
+def specialChar():
+    return random.choice(string.punctuation)
+
+
+def letter():
+    if not getrandbits(1):
+        letter = string.upper(random.choice(string.ascii_letters))
+    else:
+        letter = string.lower(random.choice(string.ascii_letters))
+    return letter
